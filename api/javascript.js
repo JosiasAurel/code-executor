cf9aafbf8ae06a2a0e75d3d1c414cba6607edcb8
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-10 10:39:05 +0100

    add express server

--- api/javascript.js
+++ api/javascript.js
@@ -1,2 +1,12 @@
 
-const { readFileSync, unlinkSync, writeFileSync } = require("fs");
+const { unlinkSync, writeFileSync } = require("fs");
+const { nanoid } = require("nanoid");
+const { spawn } = require("child_process");
+
+const code = "console.log('Hello from code executor')";
+
+let fileName = `./api/${nanoid(9)}.js`;
+
+writeFileSync(fileName, code);
+
+let file = spawn(`node`, [fileName]).stdout.on("data", data => console.log(data.toString()));
