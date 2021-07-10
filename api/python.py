8432b0a9c7cb42cc4ce5b2e00d28fb0ade76e932
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-10 11:17:16 +0100

    move codes file to /tmp/

--- api/python.py
+++ api/python.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @app.post("/")
 async def exec_code(request: Request):
     req = await request.json()
-    filename = f"{secrets.token_urlsafe(9)}.py"
+    filename = f"/tmp/{secrets.token_urlsafe(9)}.py"
     with open(filename, "w") as pycode:
         pycode.write(req["codes"])
     result = subprocess.check_output(f"python {filename}")
